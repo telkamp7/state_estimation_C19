e0cb23dab75493e7c2a47b5895ee799fd668fba2
Kasper Telkamp Nielsen <46709174+telkamp7@users.noreply.github.com>
2022-11-08 13:45:17 +0100

    Extracted raw data

---
+++ src/data/make_dataset.R
@@ -0,0 +1,26 @@
+# Load in libraries
+library(rvest)
+
+# Extract raw files to destination
+exdir_raw <- "C:/GIT/state_estimation_C19/data/raw/"
+
+# URL for the data page
+rvest_url <- "https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata"
+
+# Instantiate website object
+webpage <- read_html(rvest_url)
+
+# Collect website attributes
+col_attr <- html_attr(html_nodes(webpage, "a"), "href")
+
+# Collect url files
+url_files <- col_attr[grep(pattern = "https://files.ssi.dk/covid19/overvagning/data/overvaagningsdata-covid19", x = col_attr)]
+
+# Find the newest data file
+newest_file <- url_files[1]
+
+# Create temporary file
+temp <- tempfile(tmpdir = exdir_raw)
+download.file(url = newest_file, destfile = temp, mode = "wb")
+unzip(zipfile = temp, files = NULL, exdir = exdir_raw)
+unlink(temp)
