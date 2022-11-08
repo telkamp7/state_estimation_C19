24e64080d223f175d851864f48339d755bbf0b95
Kasper Telkamp Nielsen <46709174+telkamp7@users.noreply.github.com>
2022-11-08 14:46:09 +0100

    Created some figures

---
+++ src/visualization/visualize.R
@@ -0,0 +1,32 @@
+# Load libraries
+library(readr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+# Load processed data
+newly_admitted <- read_rds(file = "C:/GIT/state_estimation_C19/data/processed/processed_data.rds")
+
+# Number of admissions separated into regions
+newly_admitted %>%
+  select(-Total) %>%
+  pivot_longer(cols = -1,
+               names_to = "Region",
+               values_to = "Admissions") %>%
+  ggplot(mapping = aes(x = Dato, y = Admissions)) +
+  geom_line() +
+  facet_wrap(facets = vars(Region), nrow = 2, ncol = 3) +
+  labs(x = "Date", y = "Newly admitted [#]") +
+  theme_bw()
+ggsave(filename = "C:/GIT/state_estimation_C19/reports/figures/newly_admitted_regions.png",
+       dpi = "retina", width = 12, height = 8, units = "in")
+
+# Number of admissions in total
+newly_admitted %>%
+  ggplot(mapping = aes(x = Dato, y = Total)) +
+  geom_line() +
+  labs(x = "Date", y = "Newly admitted [#]") +
+  theme_bw()
+ggsave(filename = "C:/GIT/state_estimation_C19/reports/figures/newly_admitted_total.png",
+       dpi = "retina", width = 8, height = 8, units = "in")
+
